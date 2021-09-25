207e61400e75e42e455283afdc90d8c187c0b2d2
Jakson Casas <jaksoncasas@gmail.com>
2021-09-25 17:48:13 -0500

    Importat IsAuthenticatedOrReadOnly y agregar en la clase User Profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -8,6 +8,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 class HelloApiView(APIView):
     #Clase Api View de prueba
@@ -116,6 +117,10 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticatedOrReadOnly
+        )
 
     #Funcion para setear el perfil del usuario que esta logeado
     def perform_create(self, serializer):
