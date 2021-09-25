8b6a59f2dc9649ea395929460cb440b8044d042f
Jakson Casas <jaksoncasas@gmail.com>
2021-09-24 23:20:07 -0500

    Registro del viewset de la vista en el router URL

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,6 +8,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet,basename='hello-viewset')
+router.register('profile',views.UserProfileViewSet) #Crear una URL de la vista
 
 urlpatterns = [
     path('hello-view',views.HelloApiView.as_view()),
